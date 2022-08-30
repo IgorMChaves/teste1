4a0d115e42fad670eaba3a43f8dafdebe8ad4b9d
IgorMChaves <igor.1904.chaves@gmail.com>
2022-08-30 11:20:12 -0300

    quebrei o codigo de todos

--- app/Http/Controllers/API/MensagemController.php
+++ app/Http/Controllers/API/MensagemController.php
@@ -3,25 +3,32 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\Mensagem;
-use App\Traits\ApiResponse;
-use Illuminate\Contracts\Cache\Store;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 
 class MensagemController extends Controller
 {
-    use ApiResponse;
     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $mensagens = Mensagem::select(['id', 'titulo', 'mensagem', 'imagem', 'created_at', 'user_id'])
+        $mensagens = Mensagem::select(['id', 'titulo', 'mensagem' , 'imagem', 'imagem', 'created_at', 'user_id'])
             ->with(['topicos:id,topico', 'user:id,name'])
             ->orderBy('created_at', 'DESC')
             ->get();
         return $this->success($mensagens);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
     /**
@@ -32,28 +39,28 @@ class MensagemController extends Controller
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $validated = $request->validate ([
             'titulo' => 'required|max:255',
             'mensagem' => 'required|max:255',
             'topico' => 'array|exists:App\Models\Topico,id'
         ]);
-        if ($validated) {
+        if($validated){
             try{
                 $mensagem = new Mensagem();
                 $mensagem->user_id = Auth::user()->id;
                 $mensagem->titulo = $request->get('titulo');
                 $mensagem->mensagem = $request->get('mensagem');
-                if ($request->get('imagem')) {
+                if ($request->get('imagem')){
                     $image_base64 = base64_decode($request->get('imagem'));
-                    Storage::disk('s3')->put($request->get('file'), $image_base64, 'public');
+                    Storage::disk('s3')->url($request->get('file'));
                     $path = Storage::disk('s3')->url($request->get('file'));
                     $mensagem->imagem = $path;
                 }
                 $mensagem->save();
                 $mensagem->topicos()->attach($request->get('topico'));
                 return $this->success($mensagem);
-            } catch (\Throwable $th) {
-                return $this->error("Erro ao cadastrar mensagem!!", 401, $th->getMessage());
+            } catch (\Throwable $th){
+                return $this->error("Erro no cadastro da mensagem", 401, $th->getMessage());
             }
         }
     }
@@ -66,12 +73,12 @@ class MensagemController extends Controller
      */
     public function show($id)
     {
-        try {
-            $mensagem = Mensagem::where('id', $id)->with('topicos')->get();
+        try{
+            $mensagem = Mensagem::where('id', $id) ->with('topicos')->get();
             return $this->success($mensagem[0]);
-        } catch (\Throwable $th) {
-            return $this->errorr("Mensagem não encontrada!!", 401, $th->getMessage());
-        }
+        } catch (\Throwable $th){
+                return $this->error("Mensagem não encontrada", 401, $th->getMessage());
+            }
     }
 
     /**
@@ -94,7 +101,35 @@ class MensagemController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
+        $validated = $request->validate ([
+            'titulo' => 'max:255',
+            'mensagem' => 'max:255',
+            'topico' => 'array|exists:App\Models\Topico,id'
+        ]);
+        if($validated){
+            try{
+                $mensagem = Mensagem::findOrFail($id);
+                if ($request->get('titulo')){
+                    $mensagem->titulo = $request->get('titulo');
+                }
+                if ($request->get('mensagem')){
+                    $mensagem->mensagem = $request->get('mensagem');
+                }
+                if ($request->get('imagem')){  
+                    $image_base64 = base64_decode($request->get('imagem'));
+                    Storage::disk('s3')->url($request->get('file'));
+                    $path = Storage::disk('s3')->url($request->get('file'));
+                    $mensagem->imagem = $path;
+                }
+                $mensagem->save();
+                if ($request->get('topico')){
+                    $mensagem->topicos()->async($request->get('topico'));
+                }
+                return $this->success($mensagem);
+            } catch (\Throwable $th){
+                return $this->error("Erro ao atualizar a mensagem", 401, $th->getMessage());
+            }
+        }
     }
 
     /**
@@ -105,6 +140,12 @@ class MensagemController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try{
+            $mensagem = Mensagem::findOrFail($id);
+            $mensagem->delete();
+            return $this->success($mensagem);
+        } catch (\Throwable $th){
+            return $this->error("Mensagem não encontrada! ", 401, $th->getMessage());
+        }
     }
 }
